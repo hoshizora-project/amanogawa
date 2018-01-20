ba756cf04039a0eea8184e8ddbb0390073647f67
amaya <mail@sapphire.in.net>
2018-01-20 21:41:23 +0900

    fix sink_file

--- src/amanogawa/plugin/sink/file/file.cpp
+++ src/amanogawa/plugin/sink/file/file.cpp
@@ -1,7 +1,9 @@
 #include "amanogawa/include/api.h"
 #include <arrow/api.h>
+#include <exception>
 #include <fstream>
 #include <iostream>
+#include <stdexcept>
 #include <string>
 #include <text/csv/ostream.hpp>
 #include <vector>
@@ -54,32 +56,48 @@ struct SinkFilePlugin : SinkPlugin {
       csv_os << text::csv::endl;
     }
 
-    // FIXME: if chunk.size > 1 then invalid, invert loops?
-    for (size_t i = 0, end = table->num_rows(); i < end; ++i) {
-      for (const auto &field : output_fields) {
-        auto field_name = field->name();
+    // FIXME...
+    const auto num_columns = output_fields.size();
+    std::vector<uint32_t> col_idxes(num_columns);
+    for (size_t i = 0; i < num_columns; ++i) {
+      const auto index =
+          table->schema()->GetFieldIndex(output_fields.at(i)->name());
+      if (index < 0) {
+        throw std::runtime_error("Invalid output field: " +
+                                 output_fields.at(i)->name());
+      }
+      col_idxes[i] = static_cast<uint32_t>(index);
+    }
 
-        const auto field_idx = table->schema()->GetFieldIndex(field_name);
-        const auto col = table->column(static_cast<int>(field_idx));
+    std::vector<size_t> curr_chunk(num_columns);
+    std::vector<size_t> curr(num_columns);
+    for (size_t i = 0, end = table->num_rows(); i < end; ++i) {
+      for (size_t j = 0; j < num_columns; ++j) {
+        const auto col = table->column(col_idxes[j]);
         const auto type = col->type()->id();
 
-        for (const auto &chunk : col->data()->chunks()) {
-          if (type == arrow::Int32Type::type_id) {
-            const auto int32_chunk =
-                std::static_pointer_cast<arrow::Int32Array>(chunk);
-            csv_os << int32_chunk->Value(i);
-          } else if (type == arrow::DoubleType::type_id) {
-            const auto float64_chunk =
-                std::static_pointer_cast<arrow::DoubleArray>(chunk);
-            csv_os << float64_chunk->Value(i);
-          } else if (type == arrow::StringType::type_id) {
-            const auto utf8_chunk =
-                std::static_pointer_cast<arrow::StringArray>(chunk);
-            csv_os << utf8_chunk->GetString(i);
-          } else {
-            logger->warn("Detected unsupported type: {}", type);
-          }
+        if (curr[j] >= col->data()->chunk(curr_chunk[j])->length()) {
+          curr[j] = 0;
+          curr_chunk[j]++;
         }
+
+        const auto chunk = col->data()->chunk(curr_chunk[j]);
+        if (type == arrow::Int32Type::type_id) {
+          const auto int32_chunk =
+              std::static_pointer_cast<arrow::Int32Array>(chunk);
+          csv_os << int32_chunk->Value(curr[j]);
+        } else if (type == arrow::DoubleType::type_id) {
+          const auto float64_chunk =
+              std::static_pointer_cast<arrow::DoubleArray>(chunk);
+          csv_os << float64_chunk->Value(curr[j]);
+        } else if (type == arrow::StringType::type_id) {
+          const auto utf8_chunk =
+              std::static_pointer_cast<arrow::StringArray>(chunk);
+          csv_os << utf8_chunk->GetString(curr[j]);
+        } else {
+          logger->warn("Detected unsupported type: {}", type);
+        }
+        curr[j]++;
       }
       csv_os << text::csv::endl;
     }
