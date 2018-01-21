595f6a1a988a0497865d332e0322ac1e563fa0d3
amaya <mail@sapphire.in.net>
2018-01-22 04:04:00 +0900

    source_xml

---
+++ src/amanogawa/plugin/source/xml/xml.cpp
@@ -0,0 +1,97 @@
+#include "amanogawa/include/api.h"
+#include <arrow/api.h>
+#include <cstdlib>
+#include <exception>
+#include <fstream>
+#include <pugixml.hpp>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+namespace amanogawa {
+namespace plugin {
+namespace source {
+namespace xml {
+struct SourceXmlPlugin : SourcePlugin {
+  std::string plugin_name() const override { return "xml"; }
+
+  std::shared_ptr<arrow::Schema> schema;
+
+  explicit SourceXmlPlugin(const std::string &id, const config_t &config)
+      : SourcePlugin(id, config) {
+    init_logger();
+
+    const auto format =
+        *format_config->get_as<std::string>(string::keyword::type);
+    const auto cols = format_config->get_table_array(string::keyword::columns);
+    std::vector<std::shared_ptr<arrow::Field>> fields;
+    for (const auto &col : *cols) {
+      fields.emplace_back(std::make_shared<arrow::Field>(
+          *col->get_as<std::string>(string::keyword::name),
+          get_arrow_data_type(
+              *col->get_as<std::string>(string::keyword::type))));
+    }
+    schema = arrow::schema(fields);
+  }
+
+  std::shared_ptr<arrow::Table> spring() const override {
+    logger->info("spring");
+
+    using namespace pugi;
+
+    const auto file_name = *config->get_as<std::string>("path");
+    // std::ifstream fs(file_name);
+
+    xml_document doc;
+    const auto result = doc.load_file(file_name.c_str());
+
+    const auto num_fields = static_cast<size_t>(schema->num_fields());
+    std::vector<std::shared_ptr<arrow::ArrayBuilder>> builders;
+    builders.reserve(num_fields);
+    for (const auto &field : schema->fields()) {
+      builders.emplace_back(get_arrow_builder(field->type()->name()));
+    }
+
+    for (const auto &el : doc) {
+      for (size_t i = 0; i < num_fields; ++i) {
+        const auto field = schema->field(i);
+        const auto type = field->type()->id();
+        const auto key = field->name();
+
+        const auto val = el.child(key.c_str()).text();
+        if (type == arrow::Int32Type::type_id) {
+          std::static_pointer_cast<arrow::Int32Builder>(builders.at(i))
+              ->Append(val.as_int());
+        } else if (type == arrow::DoubleType::type_id) {
+          std::static_pointer_cast<arrow::DoubleBuilder>(builders.at(i))
+              ->Append(val.as_double());
+        } else if (type == arrow::StringType::type_id) {
+          std::static_pointer_cast<arrow::StringBuilder>(builders.at(i))
+              ->Append(val.as_string());
+        } else {
+          logger->warn("Detected unsupported type: {}", type);
+        }
+      }
+    }
+
+    std::vector<std::shared_ptr<arrow::Column>> columns;
+    columns.reserve(num_fields);
+    for (size_t i = 0; i < num_fields; ++i) {
+      std::shared_ptr<arrow::Array> array;
+      builders.at(i)->Finish(&array);
+      columns.emplace_back(std::make_shared<arrow::Column>(
+          schema->field(static_cast<int>(i)), array));
+    }
+
+    return arrow::Table::Make(schema, columns);
+  }
+};
+
+__attribute__((visibility("default"))) extern "C" source_plugin_t
+get_plugin(const std::string &id, const config_t &config) {
+  return std::make_shared<SourceXmlPlugin>(id, config);
+}
+} // namespace xml
+} // namespace source
+} // namespace plugin
+} // namespace amanogawa
